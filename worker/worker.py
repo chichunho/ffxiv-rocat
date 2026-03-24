81d9d0df7a483b5fb789244755b2c7c813ced8ef
chi_chunho <chunho1961@gmail.com>
2026-03-24 19:35:53 +0800

    bug fix

--- worker/worker.py
+++ worker/worker.py
@@ -3,3 +3,10 @@ from typing import Protocol
 
 class Worker(Protocol):
     async def start(self): ...
+
+
+class Cancellable(Protocol):
+    def cancel(self): ...
+
+
+class CancellableWorker(Worker, Cancellable): ...
